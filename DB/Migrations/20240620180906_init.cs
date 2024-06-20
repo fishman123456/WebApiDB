f0cfb2d162f9c368ca018a4ad5cc0f4cbe86db6c
fishman123456 <den9152@yandex.ru>
2024-06-21 01:19:13 +0700

    1-18

---
+++ DB/Migrations/20240620180906_init.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiDB.db.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReqestDatas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReceiveTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CompleteTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HttpMethod = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusCode = table.Column<int>(type: "int", nullable: false),
+                    URI = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReqestDatas", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReqestDatas");
+        }
+    }
+}
---
+++ DB/ReqestData.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApiDB.DB
+{
+    public class ReqestData
+    {
+        public int id { get; set; }
+        public DateTime ReceiveTime { get; set; }
+        public DateTime CompleteTime { get; set; }
+        public string HttpMethod { get; set; }
+        public int StatusCode { get; set; }
+        public string URI { get; set; }
+
+        public ReqestData() { }
+
+        public override string ToString()
+        {
+            return ($"{id} : {HttpMethod} => { StatusCode}");
+        }
+    }
+}
