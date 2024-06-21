2a78b29a48cd51c71535017a02c0f65dcdd645e2
Denis Borisovich Fishman <106389581+fishman123456@users.noreply.github.com>
2024-06-21 17:01:39 +0700

    два метода сделать

--- DB/ReqestData.cs
+++ DB/ReqestData.cs
@@ -3,17 +3,17 @@
     public class ReqestData
     {
         public int id { get; set; }
-        public DateTime ReceiveTime { get; set; }
-        public DateTime CompleteTime { get; set; }
-        public string HttpMethod { get; set; }
-        public int StatusCode { get; set; }
-        public string URI { get; set; }
+        public DateTime ReceiveTime { get; set; }                      // время получения запроса
+        public DateTime CompleteTime { get; set; }                    // время завершения обработки запроса
+        public string HttpMethod { get; set; } = string.Empty;       // метод запроса
+        public int StatusCode { get; set; }                         // код ответа
+        public string URI { get; set; } = string.Empty;            // URI = URL + URN
 
         public ReqestData() { }
 
         public override string ToString()
         {
-            return ($"{id} : {HttpMethod} => { StatusCode}");
+            return ($"{id} : {HttpMethod} {URI} => { StatusCode}");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Http.Extensions;
+using WebApiDB.DB;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>();
 var app = builder.Build();
 
 // обработчики 21-06-2024
@@ -6,12 +11,26 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/ping", () => new  { Massage = "pong", Time = DateTime.UtcNow });
 
 // middleware
-app.Use(async (context, next) =>
+app.Use(async (HttpContext context, RequestDelegate next) =>
 {
+    DateTime receiveTime = DateTime.UtcNow;
+    // достать сервис из iok контейнера
+    ApplicationDbContext db = context.RequestServices.GetService<ApplicationDbContext>();
     // дествие до обработчика
     Console.WriteLine($"до обработчика {DateTime.Now}");
+
     await(next(context));
     // дествие после обработчика
+    ReqestData reqestData = new ReqestData()
+    {
+        ReceiveTime = receiveTime,
+        CompleteTime = DateTime.UtcNow,
+        HttpMethod = context.Request.Method,
+        StatusCode = context.Response.StatusCode,
+        URI = context.Request.GetEncodedUrl()
+    };
+    await db.ReqestDatas.AddAsync(reqestData);
+    await db.SaveChangesAsync();
     Console.WriteLine($"после обработчика {DateTime.Now}");
 });
 app.Run();
